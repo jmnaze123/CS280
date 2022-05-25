0adc3a5d38a0ed15949beb0b7ccc79c8214b4b8c
johnny <jmnaze123@gmail.com>
2022-05-25 11:33:54 -0400

    updates

--- KnightsTourProjectOne/c++/main.cc
+++ KnightsTourProjectOne/c++/main.cc
@@ -1,17 +1,71 @@
-// This is my first time with c/c++
-// I've only learned java and python
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
-#define size = 8;
+#define size 8
 
-void solve() {
-    cout << "Solution:\n";
+void print(int [][size]);
+bool solve(int[][size],int,int,int);
 
+int main(){
 
-}
+    int i,j,row,col;
+    int board[size][size];
 
-int main() {
-    solve();
+    for(i=0;i<size;i++){
+        for(j=0;j<size;j++){
+            board[i][j]=0;
+        }
+    }
+
+    solve(board,0,0,0);
+
     return 0;
 }
+void print(int board[][size]){
 
+    int i,j;
+    
+    for (i=0;i<size;i++){
+
+        for(j=0;j<size;j++){
+
+            cout<<setw(4)<<board[i][j];
+        }
+
+        cout<<endl;
+        cout<<endl;
+    }
+}
+
+bool solve(int board[][8],int m,int n,int last){
+
+    int i,j,mpi,npj;
+    board[m][n]=++last;
+    
+    if(last==size*size){
+
+        print(board);
+        return true;
+    }
+
+    for(i=-2;i<=2;i++){
+        if(i!=0){
+            for(j=-2;j<=2;j++){
+                if(j!=0&&(i+j)%2!=0){
+                    mpi=m+i;
+                    npj=n+j;
+
+                    if( mpi >= 0 && mpi < size && 
+                        npj >= 0 && npj < size &&
+                        board[mpi][npj] == 0 && solve(board,mpi,npj,last)){
+
+                        return true;
+                    }
+                }
+            }
+        }
+    }
+    board[m][n]=0;
+    return false;
+}
--- KnightsTourProjectOne/java/Main.java
+++ KnightsTourProjectOne/java/Main.java
@@ -85,7 +85,6 @@ public class Main{
             for (int col = 0; col < size; col++) {
 
                 // nice formatting   :)
-                // dont know if i'll be able to do this in c++ 
                 if(board[row][col] == 1){
                     System.out.print("\u001B[31m  0" + board[row][col] + "\u001B[0m");
                 } else if (board[row][col] <= 5){
