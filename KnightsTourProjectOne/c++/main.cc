51fb48f9f6c4203d1b4896496e640c1f562db1ad
johnny <jmnaze123@gmail.com>
2022-05-25 10:46:16 -0400

    created main.cc

---
+++ KnightsTourProjectOne/c++/main.cc
@@ -0,0 +1,17 @@
+// This is my first time with c/c++
+// I've only learned java and python
+#include <iostream>
+using namespace std;
+#define size = 8;
+
+void solve() {
+    cout << "Solution:\n";
+
+
+}
+
+int main() {
+    solve();
+    return 0;
+}
+
