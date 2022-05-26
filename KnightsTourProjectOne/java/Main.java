7998b311f428dfe90d10565af00e7d7b7d6faa0f
johnny <jmnaze123@gmail.com>
2022-05-26 10:17:19 -0400

    updated main.java and created simple version

--- KnightsTourProjectOne/java/Main.java
+++ KnightsTourProjectOne/java/Main.java
@@ -1,66 +1,86 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
+    private static int x1, y1, size, recurCalls;
+    private static int[][] board;
+    private static int xMoves[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
+    private static int yMoves[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
+    private static String formatRequest;
 
-    private static int size = 8;
-    private static int[][] board = new int[size][size];
-    private static String ans;
+    public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+       
+        System.out.print("\nEnter a board size (1,7, or 8): ");
+        size = Integer.parseInt(sc.nextLine());
 
-    public static boolean solve() {
-        System.out.println();
-        System.out.println("Solution:");
+        if (!(size == 1 || size == 7 || size == 8)) {
 
-        for (int x = 0; x < size; x++) {
-
-            for (int y = 0; y < size; y++) {
-
-                board[x][y] = -1;
-            }
+            sc.close();
+            System.err.println("Please rerun and enter a valid board size.");
+            System.exit(0);
         }
-
-        int xMoves[] = { 2, 1, -1, -2, -2, -1, 1, 2 };
-        int yMoves[] = { 1, 2, 2, 1, -1, -2, -2, -1 };
-        board[0][0] = 1;
         
-        if (!move(0, 0, 1, xMoves, yMoves)) {
+        System.out.println("\nEnter starting X and Y (1-" + size + ") ");
+        System.out.print("X: ");
+        x1 = Integer.parseInt(sc.nextLine());
+        System.out.print("Y: ");
+        y1 = Integer.parseInt(sc.nextLine());
 
-            System.out.println("No solution.");
-            return false;
-        } else {
-            
-            printBoard();
+        if (x1 < 1 || x1 > size ||
+            y1 < 1 || y1 > size) {
+
+                sc.close();
+                System.err.println("Please rerun and enter a valid starting position.");
+                System.exit(0);
         }
+        
+        System.out.println("\nNice formatting? (Y/N) ");
+        formatRequest = sc.nextLine();
+        sc.close();
+        System.out.println();
 
-        return true;
+        x1--; y1--;
+        board = new int[size][size];
+        board[x1][y1] = 1;
+        solve();
     }
 
 
-    private static boolean move(int x, int y, int moveNumber, int xMoves[],int yMoves[]) {
+    private static void solve() {
+        System.out.println("Solution:");
+        if(!move(x1,y1,1)){
 
+            System.err.println("No solution.");
+        } else {
+            printBoard();
+        }
+    }
+
+
+    private static boolean move(int x, int y, int number) {
         int nextX, nextY;
         
-        if (moveNumber == size * size) {
-
+        if (number == size * size){
             return true;
         }
- 
-        for (int i = 0; i < size; i++) {
 
-            nextX = x + xMoves[i];
-            nextY = y + yMoves[i];
-            
+        for (int i = 0; i < size; i++) {
+            nextX = xMoves[i] + x;
+            nextY = yMoves[i] + y;
+
             if (valid(nextX, nextY)) {
 
-                board[nextX][nextY] = moveNumber + 1;
-                
-                if (move(nextX, nextY, moveNumber + 1, xMoves, yMoves)) {
+                board[nextX][nextY] = number + 1;
+
+                recurCalls++;
+                if (move(nextX, nextY, number + 1)) {
 
                     return true;
                 } else {
 
-                    board[nextX][nextY] = -1;
+                    board[nextX][nextY] = 0;
                 }
             }
         }
@@ -71,20 +91,19 @@ public class Main{
 
     private static boolean valid(int x, int y) {
 
-        if (x >= 0 && x < size && 
-            y >= 0 && y < size && 
-            board[x][y] == -1) {
-            
-            return true;
-        }
+        if (x >= 0 && y >= 0 &&
+            x < size && y < size &&
+            board[x][y] == 0) {
+
+                return true;
+            }
 
         return false;
     }
 
+
     private static void printBoard() {
-
-        if (ans.equalsIgnoreCase("y")) {
-
+        if (formatRequest.equalsIgnoreCase("y")) {
             for (int row = 0; row < size; row++) {
 
                 for (int col = 0; col < size; col++) {
@@ -96,9 +115,9 @@ public class Main{
                         System.out.print("\u001B[32m  0" + board[row][col] + "\u001B[0m");
                     } else if (board[row][col] < 10){
                         System.out.print("  0" + board[row][col]);
-                    } else if (board[row][col] >= 59 && board[row][col] != 64){
+                    } else if (board[row][col] >= size*size-5 && board[row][col] != size*size){
                         System.out.print("\u001B[33m  " + board[row][col] + "\u001B[0m");
-                    } else if (board[row][col] == 64){
+                    } else if (board[row][col] == size*size){
                         System.out.print("\u001B[31m  " + board[row][col] + "\u001B[0m");
                     } else {
                         System.out.print("  " + board[row][col]);
@@ -107,27 +126,20 @@ public class Main{
 
                 System.out.println();System.out.println();
             }
-
         } else {
 
             for (int row = 0; row < size; row++) {
 
                 for (int col = 0; col < size; col++) {
-
                     System.out.print("  " + board[row][col]);
                 }
-
                 System.out.println();
             }
         }
 
-    }
+        String rc = String.format("%,d", recurCalls);
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Nice formatting? (Y/N)");
-        ans = sc.nextLine();
-        solve();
-        sc.close();
+        System.out.println("\nRecursive calls: " + rc + "\n\n");
+        System.out.println("Project One: Knight's Tour\nBy: John Mayo\n");
     }
 }
---
+++ KnightsTourProjectOne/java/NoScanner.java
@@ -0,0 +1,41 @@
+public class NoScanner {
+    private static int recurCalls;
+    private static int[][] board = new int[8][8];
+    private static int xMoves[] = {2,1,-1,-2,-2,-1,1,2}, yMoves[] = {1,2,2,1,-1,-2,-2,-1};
+    public static void main(String[] args) {
+        board[0][0] = 1;
+        solve();
+        System.out.println(recurCalls);
+    }
+    private static void solve() {
+        System.out.println("Solution:");
+        if(!move(0,0,1)){
+            System.err.println("No solution.");
+        } else printBoard();
+    }
+    private static boolean move(int x, int y, int number) {
+        int nextX, nextY;
+        if (number == 64) return true;
+        for (int i = 0; i < 8; i++) {
+            nextX = xMoves[i] + x;
+            nextY = yMoves[i] + y;
+            if (valid(nextX, nextY)) {
+                board[nextX][nextY] = number + 1;
+                recurCalls++;
+                if (move(nextX, nextY, number + 1)) return true; 
+                else board[nextX][nextY] = 0;
+            }
+        }
+        return false;
+    }
+    private static boolean valid(int x, int y) {
+        return x >= 0 && y >= 0 && x < 8 && y < 8 && board[x][y] == 0;
+    }
+    private static void printBoard() {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++)
+                System.out.print("   " + board[row][col]);
+            System.out.println();
+        }
+    }
+}
