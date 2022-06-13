5d2369e6a3d47a84cbe6c99c9f5d76191794b9e8
johnny <jmnaze123@gmail.com>
2022-06-13 09:16:30 -0400

    project 3

--- HeapsortProjectThree/srtheap.c
+++ HeapsortProjectThree/srtheap.c
@@ -1,57 +1,48 @@
-// gcc -std=c99 -DRAND -DPRNT -DTYPE=(float | double) -D(BUBB | HEAP | INSR | MERG) *.c
+#include <stdlib.h>
+#include <string.h>
+#include "srt.h"
 
-#include <stdio.h>
+void heapify(char *array, size_t nelem, size_t size, size_t parent, int (*compar)(const void *, const void *));
 
-void main()
-{
-int heapArray[10], length, i, j, c, rootelement, tempArray;
+void srtheap(void *base, size_t nelem, size_t size, int (*compar)(const void *, const void *)) {
+    
+    char *array;
+    size_t i;
+    array = malloc((size + 1) * nelem);
+    memcpy(array + size, base, nelem * size);
 
-printf("\n Enter length of elements :");
-scanf("%d", &length);
-printf("\n Enter the lengths : ");
-for (i = 0; i < length; i++)
-scanf("%d", &heapArray[i]);
-for (i = 1; i < length; i++)
-{
-c = i;
-do
-{
-rootelement = (c - 1) / 2; 
-if (heapArray[rootelement] < heapArray[c]) /* to create MAX heapArray array */
-{
-tempArray = heapArray[rootelement];
-heapArray[rootelement] = heapArray[c];
-heapArray[c] = tempArray;
+    for (i = nelem / 2; i >= 1; i--) {
+
+        heapify(array, nelem, size, i, compar);
+    }
+
+    for (i = nelem; i > 1; i--) {
+
+        swap(array + size, array + i * size, size);
+        heapify(array, i - 1, size, 1, compar);
+    }
+
+    memcpy(base, array + size, nelem * size);
+    free(array);
 }
-c = rootelement;
-} while (c != 0);
-}
 
-printf("heapArray array\t\t:");
-for (i = 0; i < length; i++)
-printf("%d\t ", heapArray[i]);
-for (j = length - 1; j >= 0; j--)
-{
-tempArray = heapArray[0];
-heapArray[0] = heapArray[j]; /* swap max element with rightmost leaf element */
-heapArray[j] = tempArray;
-rootelement = 0;
-do 
-{
-c = 2 * rootelement + 1; /* left lengthde of rootelement element */
-if ((heapArray[c] < heapArray[c + 1]) && c < j-1)
-c++;
-if (heapArray[rootelement]<heapArray[c] && c<j) /* again rearrange to max heapArray array */
-{
-tempArray = heapArray[rootelement];
-heapArray[rootelement] = heapArray[c];
-heapArray[c] = tempArray;
-}
-rootelement = c;
-} while (c < j);
-} 
-printf("\nThe sorted array is\t:");
-for (i = 0; i < length; i++)
-printf("%d\t", heapArray[i]);
 
+void heapify(char *array, size_t nelem, size_t size, size_t parent, int (*compar)(const void *, const void *)) {
+    
+    size_t child;
+    while ((child = 2 * parent) <= nelem) {
+
+        if (child + 1 <= nelem && compar(array + child * size, array + (child + 1) * size) < 0) {
+
+            child = child + 1;
+        }
+            if (compar(array + parent * size, array + child * size) < 0) {
+
+                swap(array + parent * size, array + child * size, size);
+                parent = child;
+            } else {
+
+                break;
+            }
+    }
 }
